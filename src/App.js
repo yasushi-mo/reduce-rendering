760cc646a6386d4e47cba71d87d74f174183f886
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2021-01-10 12:35:07 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+const Child = React.memo(props => {
+  console.log("render Child");
+  return <button onClick={props.handleClick}>Child</button>
+});
+
 function App() {
+  console.log("render App");
+  const [count, setCount] = React.useState(0);
+  // const [count2, setCount2] = React.useState(0);
+
+  const handleClick = React.useCallback(() => {
+    console.log("click");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>countup App count</button>
+      {/* <button onClick={() => setCount2(count2 + 1)}>countup Child count</button> */}
+      {/* <p>App: {count}</p> */}
+      {/* <Child count={count2} /> */}
+      <Child handleClick={handleClick} />
+    </>
   );
 }
 
